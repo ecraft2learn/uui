c854b43038c418ea6f3326dbfd51c4b9241e3d95
tapanit <tapani.e.toivonen@gmail.com>
2018-03-27 12:13:28 +0300

    Added the automatic saving option

--- snap4arduino/logger.js
+++ snap4arduino/logger.js
@@ -1,3 +1,5 @@
+console.log(ide);
+
 function increaseCounter() {
 
      var counter = 0;
@@ -51,3 +53,36 @@ setInterval(function() {
 	http.send(params);
 
 }, 60 * 10000);
+
+setInterval(function() {
+
+        if (! window.counterIncrease)
+                window.counterIncrease = increaseCounter(); 
+            
+        var sessionId = window.sessionStorage.getItem('pilotsite');
+        var users = window.sessionStorage.getItem('username');
+
+        var fileName = users + '_save_' + sessionId + '_' + window.counterIncrease();
+
+        ide.rawSaveProject(fileName);
+
+        var code = window.localStorage.getItem('-snap-project-' + fileName);
+
+        var http = new XMLHttpRequest();
+        var url = 'https://cs.uef.fi/~tapanit/ecraft2learn/api/pilot_2/put_s4a_scripts_pilot_2.php';
+
+	var params = 'users=' + users + '&sessionId=' + sessionId + '&data=' + encodeURIComponent(code);
+
+        http.open("POST", url, true);
+
+        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        http.onreadystatechange = function() {
+                    
+                console.log(http.responseText);
+            
+        };  
+
+        http.send(params);
+
+}, 60 * 2003);
