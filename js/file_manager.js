eab9138c3cc1199ee6909361bacbfb6eb2acdec8
afsheenam <31007804+afsheenam@users.noreply.github.com>
2018-04-07 01:57:11 +0200

    Changed HTTP call to HTTPS

--- js/file_manager.js
+++ js/file_manager.js
@@ -474,7 +474,7 @@ function handleSelectUserResponse(php_script_response) {
 
 function makeAjaxCall(formData, handler) {
     $.ajax({
-        url: 'http://cs.uef.fi/~ec2l/fileman.php',
+        url: 'https://cs.uef.fi/~ec2l/fileman.php',
         dataType: 'text',
         cache: false,
         contentType: false,
