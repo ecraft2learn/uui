131e16d8a99ba7b354e59d0badda0a6477ecd5d8
afsheenam <31007804+afsheenam@users.noreply.github.com>
2018-04-07 01:48:32 +0200

    Bug fix with PHP server address

--- js/file_manager.js
+++ js/file_manager.js
@@ -474,7 +474,7 @@ function handleSelectUserResponse(php_script_response) {
 
 function makeAjaxCall(formData, handler) {
     $.ajax({
-        url: 'http://localhost:/fileman/fileman.php',
+        url: 'http://cs.uef.fi/~ec2l/fileman.php',
         dataType: 'text',
         cache: false,
         contentType: false,
