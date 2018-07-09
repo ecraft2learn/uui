2f432e6dc29a27bf7de61cdaba953b35fca00cc9
phlindstedt <plindstedt@troop.se>
2018-07-09 12:07:52 +0200

    update ajax to async

--- js/file_manager.js
+++ js/file_manager.js
@@ -564,7 +564,7 @@ function makeAjaxCall(formData, handler) {
         processData: false,
         data: formData,
         type: 'post',
-        async: false,
+        async: true,
         //success: function(php_script_response){
         //    $('#result').append(php_script_response + "<br/>");
         //}
