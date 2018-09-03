d239b880748f65fde927b3c0d40e19991bc4f0a1
Victor Heijler <victor@heijler.se>
2018-09-04 00:34:35 +0200

    Notification syntax update

--- lnu/sharing/js/my-work-gridview.js
+++ lnu/sharing/js/my-work-gridview.js
@@ -152,18 +152,22 @@ function stopSharingDialog(id,message) {
     stopSharing(id,function (result) {
         // console.log(result);
          if(result==="success"){
-             $.Notify({
-                 caption: 'Success',
-                 content: message,
-                 type:    null
-             });
+            Metro.notify.create(message, "Success", {});
+             // $.Notify({
+             //     caption: 'Success',
+             //     content: message,
+             //     type:    null
+             // });
          }
          else{
-             $.Notify({
-                 caption: 'Error',
-                 content: 'Some issues occur, please try again later.',
-                 type:    'alert'
-             });
+            Metro.notify.create("Work could not be unshared, some issue occurred, please try again later", "Error", {
+                cls: "alert"
+            })
+             // $.Notify({
+             //     caption: 'Error',
+             //     content: 'Some issues occur, please try again later.',
+             //     type:    'alert'
+             // });
          }
     });
 }
