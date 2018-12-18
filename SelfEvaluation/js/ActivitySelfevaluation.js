7d68e899a09fbffc2d88a010c6dc0faff8365ac9
phlindstedt <plindstedt@troop.se>
2018-12-18 20:39:57 +0100

    Fixed styling

--- SelfEvaluation/js/ActivitySelfevaluation.js
+++ SelfEvaluation/js/ActivitySelfevaluation.js
@@ -17,10 +17,10 @@ function setActivityCategories(result, status) {
             html += "<div id=\"category_" + riga.CategoryId + "-criterias\" class=\"cell-sm-6 cell-md-5\" style=\"padding-left: 0px; padding-right:opx;\"> </div>";
 
             //students remarks
-            html += "<div class=\"cell-sm-4 cell-md-5 form-group\" > <h4><label for=\"category_" + riga.CategoryId + "_remark\">Students remarks:</label><h4>  <textarea name=\"remark\" id=\"category_" + riga.CategoryId + "_remark\" rows=\"5\" style=\"width:100%;\" placeholder=\"Students remarks\" data-role=\"textarea\"></textarea>    </div>";
+            html += "<div class=\"cell-sm-4 cell-md-5 form-group\" > <label for=\"category_" + riga.CategoryId + "_remark\">Students remarks:</label>  <textarea name=\"remark\" id=\"category_" + riga.CategoryId + "_remark\" rows=\"5\" style=\"width:100%;\" placeholder=\"Students remarks\" data-role=\"textarea\"></textarea>    </div>";
 
             //self-evaluation
-            html += "<div class=\"cell-sm-2 form-group\"> <h4> <label for=\"category_" + riga.CategoryId + "_selfEvaluation\">Self-evaluation:</label></h4> <select name=\"selfEvaluation\" class=\"selfevaluation\" id=\"category_" + riga.CategoryId + "_selfEvaluation\" placeholder=\"self-evaluation\" required='true' data-role=\"select\" style=\"width:100%;\"><option value=''></option> <option value='1'>1</option> <option value='2'>2</option> <option value='3'>3</option> <option value='4'>4</option> <option value='5'>5</option> </select>";
+            html += "<div class=\"cell-sm-2 form-group\"> <label for=\"category_" + riga.CategoryId + "_selfEvaluation\">Self-evaluation:</label> <select name=\"selfEvaluation\" class=\"selfevaluation\" id=\"category_" + riga.CategoryId + "_selfEvaluation\" placeholder=\"self-evaluation\" required='true' data-role=\"select\" style=\"width:100%;\"><option value=''></option> <option value='1'>1</option> <option value='2'>2</option> <option value='3'>3</option> <option value='4'>4</option> <option value='5'>5</option> </select>";
             html += "</div> </div>";
 
             //teacher evaluation
@@ -70,7 +70,7 @@ function addCriterias(result, status) {
     $.each(gruppedCriterias, function (index, criteria) {
         html += "<div class=\"row form-group\" style=\"width: 100%; padding-bottom: 5px; padding-right: 0px; padding-left: 0px; margin:0px; \">";
         //html += "<div class=\"col-md-8\">" + criteria.Name + "</div>  <div class=\"col-md-4\">  <select id=\"category_" + criteria.Category + "-criteria_" + criteria.CritId + "-value\" class=\"form-control\"> <option value=\"\">-</option>";
-        html += "<h4><label for=\"category_" + criteria.Category + "-criteria_" + criteria.CritId + "_value\">" + criteria.Name + "</label> </h4> <select id=\"category_" + criteria.Category + "-criteria_" + criteria.CritId + "_value\" name=\"criteria\"  data-role=\"select\" style=\"width:100%;\"> <option value=\"\">-</option>";
+        html += "<label for=\"category_" + criteria.Category + "-criteria_" + criteria.CritId + "_value\">" + criteria.Name + "</label>  <select id=\"category_" + criteria.Category + "-criteria_" + criteria.CritId + "_value\" name=\"criteria\"  data-role=\"select\" style=\"width:100%;\"> <option value=\"\">-</option>";
 
         for (i = 0; i < criteria.CritValues.length; i++) {
             html += "<option value=\"" + criteria.CritValues[i] + "\">" + criteria.CritValues[i] + "</option>";
