150c1a34c2d72caec7210150f926dc6528fcd3d1
Tapani Toivonen <tapani.e.toivonen@gmail.com>
2019-05-17 10:09:28 +0300

    updates search filter

--- uef/search/js/ui.js
+++ uef/search/js/ui.js
@@ -110,7 +110,7 @@ function searchData(event) {
 
 			for (let word in data) {
 
-				if (lowerCase.indexOf(word) > -1 && word.length > 1) {
+				if (lowerCase === word || (lowerCase.indexOf(word) > - 1 && (lowerCase.length - 1 === word.length || lowerCase.length - 2 === word.length))) {
 
 					alert('Cannot use such a profane search term. You have a word ' + word + ' in your query');
 					canSearch = false;
